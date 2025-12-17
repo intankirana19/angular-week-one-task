0286fa6ba05e8e8bcd18ebd65adf89a7bee1f63f
intankirana19 <intankirana19@gmail.com>
2025-12-17 21:26:56 +0700

    move global styles & add wildcard

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,10 +2,12 @@ import { Routes } from '@angular/router';
 import { ContactFormComponent } from './pages/contact-form/contact-form.component';
 import { HomeComponent } from './pages/home/home.component';
 import { ThankYouComponent } from './pages/thank-you/thank-you.component';
+import { NotFoundComponent } from './pages/not-found/not-found.component';
 
 export const routes: Routes = [
   { path: '', redirectTo: 'home', pathMatch: 'full' },
   { path: 'home', component: HomeComponent },
   { path: 'contact-form', component: ContactFormComponent },
-  { path: 'thank-you', component: ThankYouComponent }
+  { path: 'thank-you', component: ThankYouComponent },
+  { path: '**', component: NotFoundComponent }
 ];
